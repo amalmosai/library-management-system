7aa0ff283327cc5dcb4f01966a311316dc6d862f
Amal43 <amalmoussa757@gmail.com>
2025-06-19 22:50:38 +0300

    feat(socket) : setup initial Socket.IO server for real-time features

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import errorhandler from './middlewares/errorHandler.js';
 import dotenv from 'dotenv';
 import { connectDB, PORT } from '../src/config/database.js';
@@ -9,31 +11,84 @@ dotenv.config();
 
 const app = express();
 
-import authRoute from './routes/auth.route.js';
-import bookRoute from './routes/book.route.js';
-import messageRoute from './routes/message.route.js';
+const httpServer = createServer(app);
 
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+
+    if (!userId) {
+        console.log('Anonymous connection attempted');
+        return socket.disconnect(true);
+    }
+
+    socket.join(userId);
+    socket.join('main_group');
+
+    console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`User ${userId} disconnected`);
+        // Clean up if needed
+    });
+});
+
+export const getIO = () => {
+    if (!io) {
+        throw new Error('Socket.io not initialized!');
+    }
+    return io;
+};
+
+/**
+ *
+ * middlewares
+ *
+ * */
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-// Logger for development
+/**
+ *
+ * Logger for development
+ *
+ * */
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// API Routes
+/**
+ *
+ * API Routes
+ *
+ * */
+import authRoute from './routes/auth.route.js';
+import bookRoute from './routes/book.route.js';
+import messageRoute from './routes/message.route.js';
+
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/book', bookRoute);
 app.use('/api/v1/messages', messageRoute);
 
-// Error handler
+/**
+ *
+ * global error handler
+ *
+ * */
 app.use(errorhandler);
 
 connectDB();
 /**
  * Start the server
  **/
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
